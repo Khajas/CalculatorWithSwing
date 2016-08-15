b19e25b6c6b91bc11c4f003a76fbb4c0c15e59d5
Anwar Siddiqui <z1778291@students.niu.edu>
2016-08-14 23:33:49 -0500

    Updated comments

--- Calculation.java
+++ Calculation.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package calc;
-
-/**
- *
+/***************************************
  * @author Khaja Anwar Ali Siddiqui
  * Northern Illinois University, IL, USA.
- */
+ ***************************************/
+package calc;
 public class Calculation extends javax.swing.JFrame {
     double firstNumber;
     double secondNumber;
